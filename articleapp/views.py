c41dcefad20ffc2bcbe6de6c999d1a392228cdc4
yun-aha <deu05230@naver.com>
2021-11-16 00:32:50 +0900

    [211116] decorator

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.utils.decorators import method_decorator
 from django.views.generic import DetailView
 
 
@@ -16,7 +17,15 @@ from static.dating_course2 import *
 def ydate(request):
     return render(request,'list.html')
 
+## decorator
+def yuser(func):
+    def wrap(request,*args,**kwargs):
+        user = request.user
+        mem_id = user.mem_no
+        return func(request,*args,**kwargs)
+    return wrap
 
+@method_decorator(yuser, name='dispatch')
 class RECOMMEND(DetailView):
     model = YMTI
     context_object_name = 'target'
@@ -24,43 +33,35 @@ class RECOMMEND(DetailView):
 
     # 첫 번째 추천(음식)
     def food(self):
-        user = self.request.user
-        mem_id = user.mem_no
         food = find_food_person(mem_id, 1)['mbti_code']
         return food
     # 두 번째 추천(로그기반)
     def log(self):
-        user = self.request.user
-        mem_id = user.mem_no
         log = find_log(mem_id,1)['mbti_code']
         return log
     # 세 번째 추천(YMTI)
     def ymti_recommend(self):
-        user = self.request.user
-        mem_id = user.mem_no
         df_ymti = find_ymti(mem_id,30)
         df_ymti = df_ymti.sort_values(by='concn', axis=0,ascending=False)
         ymti_recommend = df_ymti[:1]['mbti_code']
         return ymti_recommend
 
 
+
 ## 1. 첫 번째 사람에 대한 장소,음식점 추천(음식 기반 사람 추천)
+@method_decorator(yuser, name='dispatch')
 class YONE(DetailView):
     model = YMTI
     context_object_name = 'target_yone'
     template_name = 'yone.html'
 
     def yone_place_date(self):
-        user = self.request.user
-        mem_id = user.mem_no
         yone_place_date = first_dating_course(mem_id,1)
         yone_place_list = []
         for i in yone_place_date:
             yone_place_list.append(i)
         return yone_place_list[0]
     def yone_place_date_link(self):
-        user = self.request.user
-        mem_id = user.mem_no
         yone_place_date = first_dating_course(mem_id,1)
         yone_place_list = []
         for i in yone_place_date:
@@ -68,16 +69,12 @@ class YONE(DetailView):
         return yone_place_list[1]
 
     def yone_food_date(self):
-        user = self.request.user
-        mem_id = user.mem_no
         yone_food_date = first_food_course(mem_id,1)
         yone_food_list = []
         for i in yone_food_date:
             yone_food_list.append(i)
         return yone_food_list[0]
     def yone_food_date_link(self):
-        user = self.request.user
-        mem_id = user.mem_no
         yone_food_date = first_food_course(mem_id,1)
         yone_food_list = []
         for i in yone_food_date:
@@ -87,22 +84,19 @@ class YONE(DetailView):
 
 
 ## 2. 두 번째 사람에 대한 장소, 음식점 추천(로그 기반 사람 추천)
+@method_decorator(yuser, name='dispatch')
 class YTWO(DetailView):
     model = YMTI
     context_object_name = 'target_ytwo'
     template_name = 'ytwo.html'
 
     def ytwo_place_date(self):
-        user = self.request.user
-        mem_id = user.mem_no
         ytwo_place_date = second_dating_course(mem_id,1)
         ytwo_place_list = []
         for i in ytwo_place_date:
             ytwo_place_list.append(i)
         return ytwo_place_list[0]
     def ytwo_place_date_link(self):
-        user = self.request.user
-        mem_id = user.mem_no
         ytwo_place_date = second_dating_course(mem_id,1)
         ytwo_place_list = []
         for i in ytwo_place_date:
@@ -110,16 +104,12 @@ class YTWO(DetailView):
         return ytwo_place_list[1]
 
     def ytwo_food_date(self):
-        user = self.request.user
-        mem_id = user.mem_no
         ytwo_food_date = second_food_course(mem_id,1)
         ytwo_food_list = []
         for i in ytwo_food_date:
             ytwo_food_list.append(i)
         return ytwo_food_list[0]
     def ytwo_food_date_link(self):
-        user = self.request.user
-        mem_id = user.mem_no
         ytwo_food_date = second_food_course(mem_id,1)
         ytwo_food_list = []
         for i in ytwo_food_date:
@@ -129,22 +119,19 @@ class YTWO(DetailView):
 
 
 ## 3. 세 번째 사람에 대한 장소, 음식점 추천(YMTI 기반 사람 추천)
+@method_decorator(yuser, name='dispatch')
 class YTHREE(DetailView):
     model = YMTI
     context_object_name = 'target_ythree'
     template_name = 'ythree.html'
 
     def ythree_place_date(self):
-        user = self.request.user
-        mem_id = user.mem_no
         ythree_place_date = third_dating_course(mem_id,1)
         ythree_place_list = []
         for i in ythree_place_date:
             ythree_place_list.append(i)
         return ythree_place_list[0]
     def ythree_place_date_link(self):
-        user = self.request.user
-        mem_id = user.mem_no
         ythree_place_date = third_dating_course(mem_id,1)
         ythree_place_list = []
         for i in ythree_place_date:
@@ -152,16 +139,12 @@ class YTHREE(DetailView):
         return ythree_place_list[1]
 
     def ythree_food_date(self):
-        user = self.request.user
-        mem_id = user.mem_no
         ythree_food_date = third_food_course(mem_id,1)
         ythree_food_list = []
         for i in ythree_food_date:
             ythree_food_list.append(i)
         return ythree_food_list[0]
     def ythree_food_date_link(self):
-        user = self.request.user
-        mem_id = user.mem_no
         ythree_food_date = third_food_course(mem_id,1)
         ythree_food_list = []
         for i in ythree_food_date:
